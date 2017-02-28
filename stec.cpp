ec9196f4a2af4204043c27a06b2adbf97bcf556b
SnorkSnork <dazyuzin@ya.ru>
2017-02-28 03:58:53 +0300

    Add progect

--- stec.cpp
+++ stec.cpp
@@ -4,7 +4,7 @@ private:
         int *data;
         char numb;
 public:
-        stack(int size){
+        stack(int size){			//constructs new stack with this capacity
                 data = new int[size];
                 numb=0;
         }
@@ -16,14 +16,14 @@ public:
                 return *this;
         }
 
-        stack(const stack &another){
+        stack(const stack &another){		//constructs new stack, the same as another one
                 *this=another;
         }
         ~stack(){
                 delete data;
         }
 
-        void push(int a){
+        void push(int a){			//inputs new integer ino a stack
                 data[numb++]=a;
 		if (numb==capacity){
 			int* tmp;
@@ -33,7 +33,7 @@ public:
 			data=tmp;
                 }
 	}
-        int pop(){
+        int pop(){				//returns last integer put in the stack
 		if (!numb){
 			printf("ERROR! STACK IS EMPTY!\n");
 			return 0;
@@ -44,21 +44,21 @@ public:
         	        return a;
         	}
 	}
-        void swap (stack &another){
+        void swap (stack &another){		//swaps two stacks
                 stack tmp(0);
                 tmp=*this;
                 *this=another;
                 another=tmp;
         }
-	bool empty (){
+	bool empty (){				//checks if stack is empty
 		if (numb) 
 			return true;
 		else return 0;
 	}
-	char size(){
+	char size(){				//returns the current size of stack
 		return numb;
 	}
-	void free (){
+	void free (){				//makes the stack empty deleting all the elements
 		while (numb>0)
 			pop();
 	}
