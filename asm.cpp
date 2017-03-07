26371c7d6d6851478e6ccac2ee51b2ceab5db925
SnorkSnork <dazyuzin@ya.ru>
2017-03-08 03:23:12 +0400

    Update asm.cpp

--- asm.cpp
+++ asm.cpp
@@ -1,33 +1,20 @@
 #include<stdio.h>
-#include <string>
-#include <string.h>
-#include <stdlib.h>
 
 const int CMDLEN=10;
 
-class comand{
-//private:
-public:
-	char* name;
-public:
-	comand(const char line[]){
-		char* name = new char[CMDLEN];
-		for (int i=0; i<CMDLEN; ++i)
-			name[i]=line[i];
-	}
-	~comand(){
-		delete name;
-	}
-	bool operator==(const comand &another){
-                bool res=true;
-		for (int i=0; i<CMDLEN; ++i)
-			if (name[i]!=another.name[i])
-				res=false;
-		if (res)
-			return true;
-		else return false;
-        }
-};
+
+bool same (char one[], char another[]){
+	int i=0;
+	int j=0;
+	for (; one[i]!='\0'; ++i);
+	for (; another[j]!='\0'; ++j);
+	if (i!=j) 
+		return false;
+	for (i=0; i<j; ++i)
+		if (one[i]!=another[i])
+			return false;
+	return true;
+}
 
 int main(){
 	FILE *input=NULL;
@@ -48,83 +35,79 @@ int main(){
 	int cmdnum=0;
 	int stop=0;
 
-	comand push("push      ");
-	comand push_ax("pushax    ");
-	comand push_bx("pushbx    ");
-	comand push_cx("pushcx    ");
-	comand push_dx("pushdx");
-	comand pop("pop");
-	comand pop_ax("popax");
-	comand pop_bx("popbx");
-	comand pop_cx("popcx");
-	comand pop_dx("popdx");
-	comand in("in");
-	comand out("out");
-	comand sum("sum");
-	comand sub("sub");
-	comand mul("mul");
-	comand div("div");
-	comand cos("cos");
-	comand sqrt("sqrt");
-	comand deg("deg");
-	comand halt("halt");
+	char push[]{'p','u','s','h','\0'};
+	char push_ax[]{'p','u','s','h','a','x','\0'};
+	char push_bx[]{'p','u','s','h','b','x','\0'};
+	char push_cx[]{'p','u','s','h','c','x','\0'};
+	char push_dx[]{'p','u','s','h','d','x','\0'};
+	char pop[]{'p','o','p','\0'};
+	char pop_ax[]{'p','o','p','a','x','\0'};
+	char pop_bx[]{'p','o','p','b','x','\0'};
+	char pop_cx[]{'p','o','p','c','x','\0'};
+	char pop_dx[]{'p','o','p','d','x','\0'};
+	char in[]{'i','n','\0'};
+	char out[]{'o','u','t','\0'};
+	char sum[]{'s','u','m','\0'};
+	char sub[]{'s','u','b','\0'};
+	char mul[]{'m','u','l','\0'};
+	char div[]{'d','i','v','\0'};
+	char cos[]{'c','o','s','\0'};
+	char sqrt[]{'s','q','r','t','\0'};
+	char deg[]{'d','e','g','\0'};
+	char halt[]{'h','a','l','t','\0'};
 	
 
 	while ((!feof(input))&&!(stop)) {
 		++cmdnum;
 		char* cmd=new char[CMDLEN];
-		fscanf(input, "%s ", cmd);
-		comand going(cmd);
-printf ("%s\n", cmd);
-printf ("%s\n", going.name);
-		delete cmd;
-		if (push==going){
-			int* numb=new int;
-			fscanf(input, "%d ", numb);
-			fprintf (prog, "10 %d ", *numb);
-			delete numb;
+		fscanf(input, "%s", cmd);
+		if (same(cmd, push)){
+			int numb;
+			fscanf(input, "%d", &numb);
+			fprintf (prog, "10 %d ", numb);
 		}
-		else if (going==push_ax)
+		else if (same(cmd, push_ax))
 			fprintf (prog, "1a ");
-		else if (pop_ax==going)
+		else if (same(cmd, pop_ax))
 			fprintf (prog, "2a ");
-		else if (push_bx==going)
+		else if (same(cmd, push_bx))
 			fprintf (prog, "1b ");
-		else if (pop_bx==going)
+		else if (same(cmd, pop_bx))
 			fprintf (prog, "2b ");
-		else if (push_cx==going)
+		else if (same(cmd, push_cx))
 			fprintf (prog, "1c ");
-		else if (pop_cx==going)
+		else if (same(cmd, pop_cx))
 			fprintf (prog, "2c ");
-		else if (push_dx==going)
+		else if (same(cmd, push_dx))
 			fprintf (prog, "1d ");
-		else if (pop_dx==going)
+		else if (same(cmd, pop_dx))
 			fprintf (prog, "2d ");
-		else if (pop==going)
+		else if (same(cmd, pop))
 			fprintf (prog, "20 ");
-		else if (going==in)
+		else if (same(cmd, in))
 			fprintf (prog, "12 ");
-		else if (going==out)
+		else if (same(cmd, out))
 			fprintf (prog, "22 ");
-		else if (going==sum)
+		else if (same(cmd, sum))
 			fprintf (prog, "1 ");
-		else if (going==sub)
+		else if (same(cmd, sub))
 			fprintf (prog, "2 ");
-		else if (going==mul)
+		else if (same(cmd, mul))
 			fprintf (prog, "3 ");
-		else if (going==div)
+		else if (same(cmd, div))
 			fprintf (prog, "4 ");
-		else if (going==cos)
+		else if (same(cmd, cos))
 			fprintf (prog, "5 ");
-		else if (going==sqrt)
+		else if (same(cmd, sqrt))
 			fprintf (prog, "6 ");
-		else if (going==deg)
+		else if (same(cmd, deg))
 			fprintf (prog, "7 ");
-		else if (going==halt){
+		else if (same(cmd, halt)){
 			fprintf (prog, "0 ");
 			stop=1;
 		}
 		else printf ("\tWARNING! \nUNKNOWN COMMAND in line %d\n", cmdnum);
+		delete cmd;
 	}
 	fclose (prog);
 	fclose (input);
